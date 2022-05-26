9079f174941290902e6b6eb60811fc88a8ea2dd6
darkoeric57 <darkoeric57@gmail.com>
2022-05-26 17:25:17 +0000

    update 101 file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,7 +15,7 @@ size_t print_listint_safe(const listint_t *head)
 	while (head)
 	{
 		diff = head - head->next;
-		num++;
+		num = num + 1;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
 			head = head->next;
